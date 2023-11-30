d26e1378627950723e50ffa59d5908bfbc747844
guoqianxiang2 <1710421473@qq.com>
2023-11-30 11:56:43 +0800

    add benchmark balance

--- Trading/calculate_profit.py
+++ Trading/calculate_profit.py
@@ -41,17 +41,25 @@ def calculate_profits(df, principal, ticker):
 
 
 if __name__ == '__main__':
-    stocks = ['AAPL', 'GOOG', 'MSFT', 'AMZN', 'BYDDF']
+    # stock list
+    stocks = ['AAPL', 'GOOG', 'MSFT', 'AMZN', 'BYDDF', 'TCEHY']
+    # sum all five stocks's balance
     sum_balance = pd.DataFrame(columns=['balance'], data=np.zeros(251).tolist())
+    # set a subplot
+    fig, ax = plt.subplots(figsize=(9, 6))
     for ticker in stocks:
         data = pd.read_csv('../Output/moving_average/' + ticker + '_moving_average.csv')
         data, profits = calculate_profits(data, 20_0000, ticker)
-
-        sum_balance['balance'] = sum_balance['balance'] + profits
+        # plot_MA(data, ticker, 20_0000)
+        if ticker == 'TCEHY':
+            ax.plot([x/20_0000 for x in profits], label='benchmark', color='blue')
+        else:
+            sum_balance['balance'] = sum_balance['balance'] + profits
     print('Total profits:${:,.2f}'.format(sum_balance['balance'][250] - 100_0000))
-    plt.plot(sum_balance['balance']/100_0000)
-    plt.title('Balance of Sum five stocks')
-    plt.savefig('../Output/balance/sum.png', dpi=300)
+    plt.plot(sum_balance['balance']/100_0000, label='five stocks', color='orange')
+    plt.title('Balance of Sum five stocks and benchmark')
+    plt.legend(loc='upper right')
+    plt.savefig('../Output/balance/sum_benchmark.png', dpi=300)
     plt.show()
 
     # 使用 'w' 模式打开文件，如果文件不存在，将会新建一个文件
